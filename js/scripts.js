7be972c473de3835103058ab833c30ab4541e35c
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-01 09:13:25 -0700

    add for loop, name array for user input

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+var number = [];
+
+userInput to counted out number
+for (var i = 0; i <= userInput; i++) {
+  number.push[i];
+}
+console.log(number);
+// if (number.includes(3)) {
+// return("I'm sorry Dave, I'm afraid I can't do that");
+// }
+// else if (number.includes(2)){
+// change all numbers to BOOP
+// }
+// else if (number includes(1)){
+// change all numbers to BEEP
+// }
+//
+
+
+$(document).ready(function(){
+  
+
+
+
+
+})
