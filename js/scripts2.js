8ea0184d4781908d0fb8aa7bd35aa9dd7a41587d
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-02 12:32:12 -0700

    seperate business and user logic with a function

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,47 +1,44 @@
 
-
-
-
-$(document).ready(function(){
-  $("form#numberator").submit(function(event){
-    event.preventDefault();
-    var numberInput = parseInt($("input#numberInput").val());
-
-    var numbers = [];
-    for (var i = 0; i <= numberInput; i++){
-      numbers.push(i);
-      console.log(numbers);
-    };
-    var numberStr = numbers.join(", ");
-
-
-    if (numberStr.includes("3")) {
-      numberStr = numberStr.replace(/3[0-9]|[0-9]3|3/g, "I'm sorry, Dave. I'm afraid I can't do that.");
-    }
-    if (numberStr.includes("2")) {
-      numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
-    }
-    if (numberStr.includes("1")) {
-      numberStr = numberStr.replace(/1[0-9]|[0-9]1|1/g, "Beep!");
-    }
-
-//this reg ex code is working, trying it in a for loop
-    // if (numberStr.includes("2")) {
-    //   numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
-    // }
+var numbers = [];
+// var response[1] = "Beep!";
+// var response[2] = "Boop!";
+// var response[3] = "I'm Sorry, Dave. I'm afraid I can't do that.";
 //
 // for loop that doesn't work
+
 //     for (var i = 3; i > 0; i--); {
 //       if (numberStr.includes("i")) {
 //         numberStr = numberStr.replace(/i[0-9]|[0-9]i|i/g, response[i]);
 //       }
 // }
 
+function numberate(num) {
 
-console.log(numberStr);
-    var result = numberStr;
+  for (var i = 0; i <= num; i++){
+    numbers.push(i);
+  };
+  var numberStr = numbers.join(", ");
+
+  if (numberStr.includes("3")) {
+    numberStr = numberStr.replace(/3[0-9]|[0-9]3|3/g, "I'm sorry, Dave. I'm afraid I can't do that.");
+  }
+  if (numberStr.includes("2")) {
+    numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
+  }
+  if (numberStr.includes("1")) {
+    numberStr = numberStr.replace(/1[0-9]|[0-9]1|1/g, "Beep!");
+  }
+  return numberStr;
+}
+
+$(document).ready(function(){
+  $("form#numberator").submit(function(event){
+    event.preventDefault();
+    var numberInput = parseInt($("input#numberInput").val());
+
+    var result = numberate(numberInput);
+
     $("ul").html("<li>" + result + "</li>")
 
   });
-
 });
