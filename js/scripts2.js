6874a5d5102029bc154cbe4d459b48a7c6d9d263
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-02 11:47:48 -0700

    add three if statements that work for instances of 1s, 2s, and 3s

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,24 +1,5 @@
 
-response[3] = "I'm sorry, Dave. I'm afraid I can't do that."
-response[2] = '"Boop!"'
-response[1] = "Beep!"
 
-for (var i = 3, i > 0; i--);
-  if (numberStr.includes("i")) {
-    numberStr = numberStr.replace(/i[0-9]|[0-9]i|i/g, response[i]);
-
-
-
-//
-// if (numberStr.includes("3")) {
-//   numberStr = numberStr.replace(/3[0-9]|[0-9]3|3/g, "I'm sorry, Dave. I'm afraid I can't do that.");
-// }
-// if (numberStr.includes("2")) {
-//   numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
-// }
-// if (numberStr.includes("1")) {
-//   numberStr = numberStr.replace(/1[0-9]|[0-9]1|1/g, "Beep!");
-// }
 
 
 $(document).ready(function(){
@@ -33,31 +14,33 @@ $(document).ready(function(){
     };
     var numberStr = numbers.join(", ");
 
+
+    if (numberStr.includes("3")) {
+      numberStr = numberStr.replace(/3[0-9]|[0-9]3|3/g, "I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+    if (numberStr.includes("2")) {
+      numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
+    }
+    if (numberStr.includes("1")) {
+      numberStr = numberStr.replace(/1[0-9]|[0-9]1|1/g, "Beep!");
+    }
+
 //this reg ex code is working, trying it in a for loop
     // if (numberStr.includes("2")) {
     //   numberStr = numberStr.replace(/2[0-9]|[0-9]2|2/g, "Boop!");
     // }
+//
+// for loop that doesn't work
+//     for (var i = 3; i > 0; i--); {
+//       if (numberStr.includes("i")) {
+//         numberStr = numberStr.replace(/i[0-9]|[0-9]i|i/g, response[i]);
+//       }
+// }
 
 
-
-
-
-
-
-    // if (numberStr.includes("3")) {
-    //   numberStr = numberStr.replace(/^3/g, "I'm sorry Dave, I'm afraid I can't do that.")
-    //
-    // }
-
-
-    //
-    // replacedStr = numberStr.replace(/1\d/g, "Beep!");
-    // console.log(replacedStr);
 console.log(numberStr);
-    // var result = ones(numberInput)
-    // $("ul").html("<li>" + result + "</li>")
-    //numberstring = numbers.join(", ");
-    //console.log(numberstring);
+    var result = numberStr;
+    $("ul").html("<li>" + result + "</li>")
 
   });
 
